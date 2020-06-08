444f642abc12bd10523819e9ae2ee6a4c617b142
Harley Macedo <harley.ip@gmail.com>
2020-06-08 09:21:43 -0300

    Mudança para Nodejs e Express

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: node index.js
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars')
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
+
+//Engine View
+app.engine('handlebars', handlebars({defaultLayout: 'main'}) )
+
+//Sets
+app.set('view engine', 'handlebars')
+
+//Uses
+app.use(express.static(__dirname + '/public'))
+app.use(methodOverride('_method', {methods: ['GET', 'POST', 'PUT', 'DELETE']} ))
+app.use(methodOverride('X-HTTP-Method-Override'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+//Rotas
+app.get('/', (req, res) => { res.render('index', {layout: null}) })
+
+//Worker do servidor
+var porta = process.env.PORT || 3000
+app.listen(porta, () => {
+    console.log('App rodando na porta: ' + porta)
+})
