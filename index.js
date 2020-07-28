e6c42508c0d0b88a6f6a9b59e0bb5095572099de
Harley Macedo <harley.ip@gmail.com>
2020-07-28 07:38:44 -0300

    Modificação de layout para multi-page

--- index.js
+++ index.js
@@ -18,8 +18,14 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 //Rota principal
 app.get('/', (req, res) => { res.render('home', {layout: null}) })
-app.get('/simposio/2021', (req, res) => { res.render('index', {layout: null}) })
+app.get('/simposio/2021', (req, res) => { res.render('sobre', {layout: null}) })
 app.get('/simposio/2021/topicos-de-interesse', (req, res) => { res.render('topicos-de-interesse', {layout: null}) })
+app.get('/simposio/2021/sobre', (req, res) => { res.render('sobre', {layout: null}) })
+app.get('/simposio/2021/datas', (req, res) => { res.render('datas', {layout: null}) })
+app.get('/simposio/2021/contato', (req, res) => { res.render('contato', {layout: null}) })
+app.get('/simposio/2021/programacao', (req, res) => { res.render('programacao', {layout: null}) })
+app.get('/simposio/2021/trabalhos', (req, res) => { res.render('trabalhos', {layout: null}) })
+app.get('/simposio/2021/inscricoes', (req, res) => { res.render('inscricoes', {layout: null}) })
 
 
 //Worker do servidor
