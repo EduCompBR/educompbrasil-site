9cb90908f2124f50d8a05305b152a8c7149ec42d
Esdras <esdraspiano@gmail.com>
2020-08-15 18:43:09 -0300

    Refatoração dos layouts
    Closes #10

--- index.js
+++ index.js
@@ -21,16 +21,36 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.get('/', (req, res) => { res.render('giec/index', {layout: 'giec/layout'}) })
 
 //EduComp 2021
-app.get('/simposio/2021', (req, res) => { res.render('sobre', {layout: null, sobre: true}) })
-app.get('/simposio/2021/topicos-de-interesse', (req, res) => { res.render('topicos-de-interesse', {layout: null, topicos: true}) })
-app.get('/simposio/2021/sobre', (req, res) => { res.render('sobre', {layout: null, sobre: true}) })
-app.get('/simposio/2021/datas', (req, res) => { res.render('datas', {layout: null, datas: true}) })
-app.get('/simposio/2021/contato', (req, res) => { res.render('contato', {layout: null, contato: true}) })
-app.get('/simposio/2021/programacao', (req, res) => { res.render('programacao', {layout: null, prog: true}) })
-app.get('/simposio/2021/wlic', (req, res) => { res.render('wlic', {layout: null, wlic: true}) })
-app.get('/simposio/2021/trabalhos', (req, res) => { res.render('trabalhos', {layout: null, trabalhos: true}) })
-app.get('/simposio/2021/inscricoes', (req, res) => { res.render('inscricoes', {layout: null, inscricoes: true}) })
-app.get('/simposio/2021/equipe', (req, res) => { res.render('equipe', {layout: null, equipe: true}) })
+app.get('/simposio/2021', (req, res) => { 
+    res.render('sobre', {layout: 'simposio/2021/layout', sobre: true}) 
+})
+app.get('/simposio/2021/topicos-de-interesse', (req, res) => { 
+    res.render('topicos-de-interesse', {layout: 'simposio/2021/layout', topicos: true}) 
+})
+app.get('/simposio/2021/sobre', (req, res) => { 
+    res.render('sobre', {layout: 'simposio/2021/layout', sobre: true}) 
+})
+app.get('/simposio/2021/datas', (req, res) => { 
+    res.render('datas', {layout: 'simposio/2021/layout', datas: true}) 
+})
+app.get('/simposio/2021/contato', (req, res) => { 
+    res.render('contato', {layout: 'simposio/2021/layout', contato: true}) 
+})
+app.get('/simposio/2021/programacao', (req, res) => { 
+    res.render('programacao', {layout: 'simposio/2021/layout', prog: true}) 
+})
+app.get('/simposio/2021/wlic', (req, res) => { 
+    res.render('wlic', {layout: 'simposio/2021/layout', wlic: true}) 
+})
+app.get('/simposio/2021/trabalhos', (req, res) => { 
+    res.render('trabalhos', {layout: 'simposio/2021/layout', trabalhos: true}) 
+})
+app.get('/simposio/2021/inscricoes', (req, res) => { 
+    res.render('inscricoes', {layout: 'simposio/2021/layout', inscricoes: true}) 
+})
+app.get('/simposio/2021/equipe', (req, res) => { 
+    res.render('equipe', {layout: 'simposio/2021/layout', equipe: true}) 
+})
 app.post('/simposio/2021/contato/email', async (req, res) => { 
     await emailServer.enviarEmail(req.body.email1, req.body.assunto1, req.body.mensagem1)
     res.redirect('/simposio/2021/contato')
