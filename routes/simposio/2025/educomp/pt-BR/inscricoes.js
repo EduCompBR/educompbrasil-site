7df8e87aaf82093e0d19f4c058ad94fe25f9b23d
Luciano J Chaves <ljerezchaves@gmail.com>
2025-02-15 11:05:03 -0300

    Nova página de inscrições, ainda sem as tabelas de valores.

--- routes/simposio/2025/educomp/pt-BR/inscricoes.js
+++ routes/simposio/2025/educomp/pt-BR/inscricoes.js
@@ -4,7 +4,7 @@ exports.inscricoes = function (req, res) {
     inscricoes: true,
     isEducomp: true,
     titulo: 'Inscrições',
-    header: { titulo: 'Definir' },
+    header: { titulo: 'Inscrições para o Educomp e SBC-EB' },
   })
 }
 
