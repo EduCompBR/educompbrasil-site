d8a403b731dc9735938bb99223d05dc933880be9
Luciano J Chaves <ljerezchaves@gmail.com>
2025-02-15 21:53:41 -0300

    Atualizando menu local na navbar.

--- routes/simposio/2025/educomp/pt-BR/local.js
+++ routes/simposio/2025/educomp/pt-BR/local.js
@@ -1,10 +1,20 @@
+exports.conheca_jf = function (req, res) {
+  res.render('simposio/2025/comum/pt-BR/local/conheca-jf', {
+    layout: 'simposio/2025/pt-BR/layout',
+    local: true,
+    isEducomp: true,
+    titulo: 'Conheça Juiz de Fora - MG',
+    header: { titulo: 'Juiz de Fora - MG' },
+  })
+}
+
 exports.como_chegar = function (req, res) {
   res.render('simposio/2025/comum/pt-BR/local/como-chegar-jf', {
     layout: 'simposio/2025/pt-BR/layout',
     local: true,
     isEducomp: true,
-    titulo: 'Como chegar',
-    header: { titulo: 'Definir' },
+    titulo: 'Como chegar em Juiz de Fora',
+    header: { titulo: 'Como chegar em Juiz de Fora' },
   })
 }
 
@@ -13,27 +23,17 @@ exports.onde_hospedar = function (req, res) {
     layout: 'simposio/2025/pt-BR/layout',
     local: true,
     isEducomp: true,
-    titulo: 'Onde se hospedar',
-    header: { titulo: 'Definir' },
+    titulo: 'Onde se hospedar em Juiz de Fora',
+    header: { titulo: 'Onde se hospedar em Juiz de Fora' },
   })
 }
 
-exports.conheca_jf = function (req, res) {
-  res.render('simposio/2025/comum/pt-BR/local/conheca-jf', {
-    layout: 'simposio/2025/pt-BR/layout',
-    local: true,
-    isEducomp: true,
-    titulo: 'Conheça Juiz de Fora',
-    header: { titulo: 'Definir' },
-  })
-}
-
-exports.conheca_ufjf = function (req, res) {
+exports.local_evento = function (req, res) {
   res.render('simposio/2025/comum/pt-BR/local/conheca-ufjf', {
     layout: 'simposio/2025/pt-BR/layout',
     local: true,
     isEducomp: true,
-    titulo: 'Conheça a UFJF',
-    header: { titulo: 'Definir' },
+    titulo: 'Local do evento - UFJF',
+    header: { titulo: 'Local do evento - UFJF' },
   })
 }
