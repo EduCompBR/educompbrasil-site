7d202af40c1a7d1d3edb7d6e5965cd4e766e04bf
Luciano J Chaves <ljerezchaves@gmail.com>
2025-02-16 19:37:14 -0300

    Página das equipes.

--- routes/simposio/2025/sbc-eb/pt-BR/forlic.js
+++ routes/simposio/2025/sbc-eb/pt-BR/forlic.js
@@ -1,5 +1,5 @@
 exports.forlic = function (req, res) {
-  res.render('simposio/2025/sbc-eb/pt-BR/chamadas/forlic', {
+  res.render('simposio/2025/sbc-eb/pt-BR/forlic', {
     layout: 'simposio/2025/pt-BR/layout',
     forlic: true,
     isSbceb: true,
