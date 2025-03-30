655292d5e43f897cf727767f2d86becfb1dec344
Luciano J Chaves <ljerezchaves@gmail.com>
2025-03-30 11:24:55 -0300

    Reordenando rotas.

--- routes/simposio/2025/sbc-eb/pt-BR/programacao.js
+++ routes/simposio/2025/sbc-eb/pt-BR/programacao.js
@@ -7,20 +7,20 @@ exports.geral = function (req, res) {
   })
 }
 
-exports.jantar = function (req, res) {
-  res.render('simposio/2025/comum/pt-BR/programacao/jantar', {
-    layout: 'simposio/2025/pt-BR/layout',
-    programacao: true,
-    isSbceb: true,
-    titulo: 'Jantar do evento',
-  })
-}
-
 exports.sede = function (req, res) {
   res.render('simposio/2025/sbc-eb/pt-BR/programacao/sede', {
     layout: 'simposio/2025/pt-BR/layout',
     programacao: true,
     isSbceb: true,
     titulo: 'Programação Juiz de Fora - MG',
+  })
+}
+
+exports.jantar = function (req, res) {
+  res.render('simposio/2025/comum/pt-BR/programacao/jantar', {
+    layout: 'simposio/2025/pt-BR/layout',
+    programacao: true,
+    isSbceb: true,
+    titulo: 'Jantar do evento',
   })
 }
