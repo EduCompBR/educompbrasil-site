20f773b168c10833a907c0f1e9310a368ebd012d
Luciano J Chaves <ljerezchaves@gmail.com>
2025-03-30 12:00:32 -0300

    Páginas para toda a programação

--- routes/simposio/2025/sbc-eb/pt-BR/programacao.js
+++ routes/simposio/2025/sbc-eb/pt-BR/programacao.js
@@ -7,12 +7,57 @@ exports.geral = function (req, res) {
   })
 }
 
-exports.sede = function (req, res) {
-  res.render('simposio/2025/sbc-eb/pt-BR/programacao/sede', {
+exports.juiz_de_fora = function (req, res) {
+  res.render('simposio/2025/sbc-eb/pt-BR/programacao/juiz-de-fora', {
     layout: 'simposio/2025/pt-BR/layout',
     programacao: true,
     isSbceb: true,
     titulo: 'Programação Juiz de Fora - MG',
+  })
+}
+
+exports.bandeirantes = function (req, res) {
+  res.render('simposio/2025/sbc-eb/pt-BR/programacao/bandeirantes', {
+    layout: 'simposio/2025/pt-BR/layout',
+    programacao: true,
+    isSbceb: true,
+    titulo: 'Programação Bandeirantes - PR',
+  })
+}
+
+exports.campo_grande = function (req, res) {
+  res.render('simposio/2025/sbc-eb/pt-BR/programacao/campo-grande', {
+    layout: 'simposio/2025/pt-BR/layout',
+    programacao: true,
+    isSbceb: true,
+    titulo: 'Programação Campo Grande - MS',
+  })
+}
+
+exports.itacoatiara = function (req, res) {
+  res.render('simposio/2025/sbc-eb/pt-BR/programacao/itacoatiara', {
+    layout: 'simposio/2025/pt-BR/layout',
+    programacao: true,
+    isSbceb: true,
+    titulo: 'Programação Itacoatiara - AM',
+  })
+}
+
+exports.salvador = function (req, res) {
+  res.render('simposio/2025/sbc-eb/pt-BR/programacao/salvador', {
+    layout: 'simposio/2025/pt-BR/layout',
+    programacao: true,
+    isSbceb: true,
+    titulo: 'Programação Salvador - BA',
+  })
+}
+
+exports.sessoes_tecnicas = function (req, res) {
+  res.render('simposio/2025/sbc-eb/pt-BR/programacao/sessoes-tecnicas', {
+    layout: 'simposio/2025/pt-BR/layout',
+    programacao: true,
+    isSbceb: true,
+    titulo: 'Sessões técnicas',
   })
 }
 
