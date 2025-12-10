87e7d0de4de49ed4af37188efdf15ca3651905a1
httsdinha <amandabalcacar@gmail.com>
2025-12-09 20:14:04 -0400

    feat: adiciona rotas para o SBC-EB 2026

---
+++ routes/simposio/2026/sbc-eb/pt-BR/home.js
@@ -0,0 +1,17 @@
+exports.home = function (req, res) {
+  res.render('simposio/2026/sbc-eb/pt-BR/home', {
+    layout: 'simposio/2026/pt-BR/layout',
+    home: true,
+    isSbceb: true,
+    titulo: 'Home',
+  })
+}
+
+exports.camisas = function (req, res) {
+  res.render('simposio/2025/comum/pt-BR/camisas', {
+    layout: 'simposio/2025/pt-BR/layout',
+    home: true,
+    isSbceb: true,
+    titulo: 'Camisas do evento',
+  })
+}
