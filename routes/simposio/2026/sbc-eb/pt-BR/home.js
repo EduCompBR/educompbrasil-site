00050b3e638b3404d8d6e5faa9cc5361a5ea9643
Hudson Silva Borges <hudsonsilbor@gmail.com>
2025-12-14 10:53:44 -0300

    refactor: remove página ainda não disponivel

--- routes/simposio/2026/sbc-eb/pt-BR/home.js
+++ routes/simposio/2026/sbc-eb/pt-BR/home.js
@@ -6,12 +6,3 @@ exports.home = function (req, res) {
     titulo: 'Home',
   })
 }
-
-exports.camisas = function (req, res) {
-  res.render('simposio/2025/comum/pt-BR/camisas', {
-    layout: 'simposio/2025/pt-BR/layout',
-    home: true,
-    isSbceb: true,
-    titulo: 'Camisas do evento',
-  })
-}
