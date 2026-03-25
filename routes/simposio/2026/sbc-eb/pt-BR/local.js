ed65bec56b9d05731bf784e2c504529be9adbecc
httsdinha <amandabalcacar@gmail.com>
2026-03-25 14:04:36 -0400

    feat: adiciona pagina de local do evento

---
+++ routes/simposio/2026/sbc-eb/pt-BR/local.js
@@ -0,0 +1,35 @@
+exports.conheca_cg = function (req, res) {
+  res.render('simposio/2026/comum/pt-BR/local/conheca-campo-grande', {
+    layout: 'simposio/2026/pt-BR/layout',
+    local: true,
+    isSbceb: true,
+    titulo: 'Conheça Campo Grande',
+  })
+}
+
+exports.como_chegar = function (req, res) {
+  res.render('simposio/2026/comum/pt-BR/local/como-chegar', {
+    layout: 'simposio/2026/pt-BR/layout',
+    local: true,
+    isSbceb: true,
+    titulo: 'Como chegar a Campo Grande',
+  })
+}
+
+exports.onde_hospedar = function (req, res) {
+  res.render('simposio/2026/comum/pt-BR/local/onde-hospedar', {
+    layout: 'simposio/2026/pt-BR/layout',
+    local: true,
+    isSbceb: true,
+    titulo: 'Onde se hospedar em Campo Grande',
+  })
+}
+
+exports.local_evento = function (req, res) {
+  res.render('simposio/2026/comum/pt-BR/local/local-evento', {
+    layout: 'simposio/2026/pt-BR/layout',
+    local: true,
+    isSbceb: true,
+    titulo: 'Conheça o local do evento',
+  })
+}
